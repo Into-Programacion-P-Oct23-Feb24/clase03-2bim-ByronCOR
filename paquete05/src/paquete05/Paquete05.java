21810a521f213f5745b126498d48091619ba3fe1
unknown <bdcordova@utpl.edu.ec>
2023-12-20 15:32:10 -0500

    cambios

---
+++ paquete05/src/paquete05/Paquete05.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Paquete05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+              
+        int arreglo3[] = new int[2];
+        for(int i = 0 ;i < arreglo3.length;i++){
+            System.out.println(arreglo3[i]);
+        }
+
+        
+ ;
+    }
+    
+}
